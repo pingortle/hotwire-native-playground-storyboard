155dcd9a3fc0801a550e921d5be9684097a3d5e9
Kaleb Lape <kaleb.lape@gmail.com>
2024-09-25 17:07:29 -0500

    Initial Commit

---
+++ hotwire-native-playground-storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hotwire-native-playground-storyboard
+//
+//  Created by Kaleb Lape on 9/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hotwire-native-playground-storyboardTests/hotwire_native_playground_storyboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  hotwire_native_playground_storyboardTests.swift
+//  hotwire-native-playground-storyboardTests
+//
+//  Created by Kaleb Lape on 9/25/24.
+//
+
+import Testing
+@testable import hotwire_native_playground_storyboard
+
+struct hotwire_native_playground_storyboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
